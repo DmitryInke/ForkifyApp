75f9f5f0683a20a979b572677a8074f91d30ae52
Dmitry Inke <angranity1998@gmail.com>
2021-10-02 14:43:17 +0300

    Implementing Search Results, implementing Error and Success Messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -14,11 +20,29 @@ const controlRecipes = async function () {
     // Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    resultsView.renderSpinner();
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    resultsView.renderError();
   }
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
